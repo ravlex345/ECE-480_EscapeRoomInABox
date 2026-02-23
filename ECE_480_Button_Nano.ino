23b970bdfc429caa6125a02922aa0adfbe7382db
Alika Sharma <sharm226@msu.edu>
2026-02-22 19:04:22 -0500

    slave code added

---
+++ ECE_480_Button_Nano.ino
@@ -0,0 +1,139 @@
+
+int pushButton1 = 14;
+int pushButton2 = 15;
+int pushButton3 = 16;
+int pushButton4 = 17;
+int pushButton5 = 18;
+
+enum buttonStates {initial,correct1, wrong1, correct2, wrong2, correct3, wrong3, correct4, wrong4};
+buttonStates state;
+
+int redLight = 6;
+int greenLight = 7;
+
+void printState(buttonStates s) {
+  switch(s) {
+    case initial: Serial.println("initial"); break;
+    case correct1: Serial.println("correct1"); break;
+    case wrong1: Serial.println("wrong1"); break;
+    case correct2: Serial.println("correct2"); break;
+    case wrong2: Serial.println("wrong2"); break;
+    case correct3: Serial.println("correct3"); break;
+    case wrong3: Serial.println("wrong3"); break;
+    case correct4: Serial.println("correct4"); break;
+    case wrong4: Serial.println("wrong4"); break;
+  }
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  
+  pinMode( pushButton1, INPUT);
+  pinMode( pushButton2, INPUT);
+  pinMode( pushButton3, INPUT);
+  pinMode( pushButton4, INPUT);
+  pinMode( pushButton5, INPUT);
+
+  pinMode (redLight , OUTPUT);
+  pinMode (greenLight , OUTPUT);
+
+  state = initial;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool value1 = digitalRead(pushButton1); 
+  bool value2 = digitalRead(pushButton2);
+  bool value3 = digitalRead(pushButton3);
+  bool value4 = digitalRead(pushButton4);
+  bool value5 = digitalRead(pushButton5);
+  
+//  Serial.println(value1);
+//  Serial.println(value2);
+//  Serial.println(value3);
+//  Serial.println(value4);
+
+  if (value5 == 0)
+  {
+    state = initial;
+  }
+printState(state);
+  Serial.println(value5);
+
+delay(300);
+
+  switch(state){
+    case initial:
+          digitalWrite(redLight, LOW);
+      digitalWrite(greenLight, LOW);
+
+      if (value1 == 0) {
+        state = correct1;
+      }
+      else if (value2 == 0 or value3 == 0 or value4 == 0){
+        state = wrong1;
+      }
+      break;
+      
+    case correct1:
+      if (value2 == 0) {
+        state = correct2;
+      }
+      else if (value1 == 0 or value3 == 0 or value4 == 0){
+        state = wrong2;
+      }
+      break;
+
+   case correct2:
+    if (value3 == 0) {
+      state = correct3;
+    }
+    else if (value2 == 0 or value2 == 0 or value4 == 0){
+      state = wrong2;
+    }
+    break;
+
+    case correct3:
+    if (value4 == 0){
+      state = correct4;
+    }
+    else if (value2 == 0 or value3 == 0 or value1 == 0){
+      state = wrong4;
+    }
+    break;
+    
+    case correct4:
+      digitalWrite(greenLight, HIGH);
+      
+      break;
+      
+    case wrong1:
+      if (value1 == 0 or value2 == 0 or value3 == 0 or value4 == 0){
+        state = wrong2;
+       }
+      break;
+
+    case wrong2:
+    
+    if (value1 == 0 or value2 == 0 or value3 == 0 or value4 == 0){
+        state = wrong3;
+       }
+       break;
+
+    case wrong3:
+      if (value1 == 0 or value2 == 0 or value3 == 0 or value4 == 0){
+        state = wrong4;
+       }
+       break;
+      
+    case wrong4:
+      digitalWrite(redLight, HIGH);
+      break;
+  }
+
+
+  
+}
---
+++ ECE_480_KeyPad_Nano.ino
@@ -0,0 +1,162 @@
+#include <Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+{'1', '2', '3', 'A'},
+{'4', '5', '6', 'B'},
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {2, 3, 4, 5};
+byte colPins[COLS] = {6, 7, 8, 9};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+// Pins
+#define GREEN_LED 10 // Correct password
+#define RED_LED 11 // Wrong password / keypress feedback
+#define RESET_BUTTON 12 // Physical reset button
+
+// Password settings
+const String PASSWORD = "247B";
+const int MAX_TRIES = 3;
+
+String enteredCode = "";
+int attempts = 0;
+bool solved = false;
+bool lockedOut = false;
+unsigned long lockoutEnd = 0;
+
+void setup() {
+Serial.begin(9600);
+pinMode(GREEN_LED, OUTPUT);
+pinMode(RED_LED, OUTPUT);
+pinMode(RESET_BUTTON, INPUT_PULLUP);
+resetSystem();
+Serial.println("=== KEYPAD TEST ===");
+Serial.print("Password: ");
+Serial.println(PASSWORD);
+Serial.println("Red flashes on keypress");
+Serial.println("Press # to submit, * to clear");
+Serial.println();
+}
+
+void loop() {
+// Check reset button
+if (digitalRead(RESET_BUTTON) == LOW) {
+delay(50);
+if (digitalRead(RESET_BUTTON) == LOW) {
+Serial.println(">>> RESET <<<");
+resetSystem();
+while (digitalRead(RESET_BUTTON) == LOW);
+}
+}
+// Check serial reset
+if (Serial.available()) {
+char cmd = Serial.read();
+if (cmd == 'r' || cmd == 'R') {
+resetSystem();
+}
+}
+// Handle lockout
+if (lockedOut && millis() > lockoutEnd) {
+lockedOut = false;
+attempts = 0;
+digitalWrite(RED_LED, LOW);
+Serial.println("Lockout ended.");
+}
+// Skip if solved or locked out
+if (solved || lockedOut) return;
+char key = keypad.getKey();
+if (key) handleKey(key);
+}
+
+void handleKey(char key) {
+Serial.print("Key: ");
+Serial.println(key);
+// RED FLASH on every keypress
+flashRedQuick();
+// Clear with *
+if (key == '*') {
+enteredCode = "";
+Serial.println("--- Cleared ---");
+return;
+}
+// Submit with #
+if (key == '#') {
+checkPassword();
+return;
+}
+// Add to code
+if (enteredCode.length() < 4) {
+enteredCode += key;
+Serial.print("Entered: ");
+Serial.println(enteredCode);
+}
+}
+
+void checkPassword() {
+Serial.print("Checking: ");
+Serial.println(enteredCode);
+if (enteredCode == PASSWORD) {
+// CORRECT! Green on, red off
+solved = true;
+digitalWrite(GREEN_LED, HIGH);
+digitalWrite(RED_LED, LOW);
+Serial.println("*** CORRECT! ***");
+victoryFlash();
+} else {
+// WRONG! Red stays on
+attempts++;
+Serial.print("WRONG! Try ");
+Serial.print(attempts);
+Serial.println("/3");
+digitalWrite(RED_LED, HIGH);
+delay(500);
+digitalWrite(RED_LED, LOW);
+// Build up: 1 flash = 1 wrong, 2 flashes = 2 wrongs, etc
+for (int i = 1; i < attempts; i++) {
+delay(200);
+digitalWrite(RED_LED, HIGH);
+delay(200);
+digitalWrite(RED_LED, LOW);
+}
+if (attempts >= MAX_TRIES) {
+lockedOut = true;
+lockoutEnd = millis() + 10000;
+digitalWrite(RED_LED, HIGH);
+Serial.println("!!! LOCKED OUT !!!");
+}
+}
+enteredCode = "";
+}
+
+void flashRedQuick() {
+// Quick red flash for keypress feedback
+digitalWrite(RED_LED, HIGH);
+delay(30);
+digitalWrite(RED_LED, LOW);
+}
+
+void resetSystem() {
+enteredCode = "";
+attempts = 0;
+solved = false;
+lockedOut = false;
+digitalWrite(GREEN_LED, LOW);
+digitalWrite(RED_LED, LOW);
+Serial.println(">>> RESET <<<");
+}
+
+void victoryFlash() {
+for (int i = 0; i < 10; i++) {
+digitalWrite(GREEN_LED, LOW);
+delay(100);
+digitalWrite(GREEN_LED, HIGH);
+delay(100);
+}
+}
+
