68b85b79cb9f0b491b0a10d4b26a7f6a5802e634
Alika Sharma <sharm226@msu.edu>
2026-04-14 21:29:40 -0400

    Faster button

--- ECE_480_Button_Nano/ECE_480_Button_Nano.ino
+++ ECE_480_Button_Nano/ECE_480_Button_Nano.ino
@@ -1,211 +1,175 @@
 #include "Wire.h"
 
+// ===================== BUTTON PINS =====================
 int pushButton1 = 16;
 int pushButton2 = 3;
 int pushButton3 = 7;
-int pushButton4 = 5;
+int pushButton4 = 12;
+int pushButton5 = 15;
 
+// Tuple-style grouping (array in Arduino)
+int buttons[4] = {pushButton1, pushButton2, pushButton3, pushButton4};
 
-int pushButton5 = 14;
-
-enum buttonStates {initial,correct1, wrong1, correct2, wrong2, correct3, wrong3, correct4, wrong4};
+// ===================== STATE =====================
+enum buttonStates {initial, correct1, wrong1, correct2, wrong2, correct3, wrong3, correct4, wrong4};
 buttonStates state;
 
-int redLight = 8;
-int greenLight =9;
+bool lastButtonState[4] = {HIGH, HIGH, HIGH, HIGH};
 
-bool PuzzleActive = false;
+// ===================== OUTPUTS =====================
+int redLight = 8;
+int greenLight = 9;
 
 #define INTERRUPT_PIN 11
 
-void printState(buttonStates s) {
-  switch(s) {
-    case initial: Serial.println("initial"); break;
-    case correct1: Serial.println("correct1"); break;
-    case wrong1: Serial.println("wrong1"); break;
-    case correct2: Serial.println("correct2"); break;
-    case wrong2: Serial.println("wrong2"); break;
-    case correct3: Serial.println("correct3"); break;
-    case wrong3: Serial.println("wrong3"); break;
-    case correct4: Serial.println("correct4"); break;
-    case wrong4: Serial.println("wrong4"); break;
-  }
-}
+bool PuzzleActive = false;
 
+// ===================== FUNCTION DECLARATIONS =====================
+void handleButtonPress(int i);
+void performFullReset();
 
+// ===================== SETUP =====================
 void setup() {
-  // put your setup code here, to run once:
-
   Serial.begin(9600);
-  
-  pinMode( pushButton1, INPUT_PULLUP);
-  pinMode( pushButton2, INPUT_PULLUP);
-  pinMode( pushButton3, INPUT_PULLUP);
-  pinMode( pushButton4, INPUT_PULLUP);
-  pinMode( pushButton5, INPUT_PULLUP);
 
-  pinMode (redLight , OUTPUT);
-  pinMode (greenLight , OUTPUT);
+  pinMode(pushButton1, INPUT_PULLUP);
+  pinMode(pushButton2, INPUT_PULLUP);
+  pinMode(pushButton3, INPUT_PULLUP);
+  pinMode(pushButton4, INPUT_PULLUP);
+  pinMode(pushButton5, INPUT_PULLUP);
 
-  pinMode( INTERRUPT_PIN, OUTPUT);
-  state = initial;
+  pinMode(redLight, OUTPUT);
+  pinMode(greenLight, OUTPUT);
+  pinMode(INTERRUPT_PIN, OUTPUT);
 
-  digitalWrite(redLight, LOW);
-  digitalWrite(greenLight, LOW);
-  digitalWrite(INTERRUPT_PIN, HIGH);
+  performFullReset();
 
-  Wire.begin(9);                // I2C slave address
-  Wire.onReceive(receiveEvent); // I2C receive handler
+  Wire.begin(9);
+  Wire.onReceive(receiveEvent);
 
-  Serial.println("Setup for puzzle 2 done");
+  Serial.println("Puzzle 2 Ready");
 }
 
+// ===================== MAIN LOOP =====================
 void loop() {
-  if (!PuzzleActive) return;
 
-  // put your main code here, to run repeatedly:
-  bool value1 = digitalRead(pushButton1); 
-  bool value2 = digitalRead(pushButton2);
-  bool value3 = digitalRead(pushButton3);
-  bool value4 = digitalRead(pushButton4);
-  bool value5 = digitalRead(pushButton5);
-      Serial.println(value1);
-    Serial.println(value2);
-    Serial.println(value3);
-    Serial.println(value4);
-        Serial.println(value5);
-
-    Serial.println();
-
-
-//  Serial.println(value1);
-//  Serial.println(value2);
-//  Serial.println(value3);
-//  Serial.println(value4);
-
-  if (value5 == 0)
-  {
-    state = initial;
+  // 1. Physical Reset Button
+  if (digitalRead(pushButton5) == LOW) {
+    performFullReset();
+    delay(200); // debounce
+    Serial.println(5);
+    return;
+    
   }
-  printState(state);
-  //Serial.println(value5);
 
-delay(300);
+  // 2. Only process if active
+  //if (!PuzzleActive) return;
 
-  switch(state){
+  // 3. Check buttons (edge detection)
+  for (int i = 0; i < 4; i++) {
+    bool currentState = digitalRead(buttons[i]);
+
+    if (currentState == LOW && lastButtonState[i] == HIGH) {
+      handleButtonPress(i);
+      delay(50); // small debounce
+      Serial.println(i);
+    }
+
+    lastButtonState[i] = currentState;
+  }
+}
+
+// ===================== BUTTON LOGIC =====================
+void handleButtonPress(int i) {
+
+  switch(state) {
+
     case initial:
-        digitalWrite(redLight, LOW);
-        digitalWrite(greenLight, LOW);
-
-      if (value1 == 0) {
-        state = correct1;
-        flashRedQuick();
-      }
-      else if (value2 == 0 or value3 == 0 or value4 == 0){
-        state = wrong1;
-        flashRedQuick();
-      }
+      if (i == 0) state = correct1;
+      else state = wrong1;
       break;
-      
+
     case correct1:
-      
-      if (value2 == 0) {
-        state = correct2;
-        flashRedQuick();
-      }
-      else if (value1 == 0 or value3 == 0 or value4 == 0){
-  
-        state = wrong2;
-        flashRedQuick();
-      }
+      if (i == 1) state = correct2;
+      else state = wrong2;
       break;
 
-   case correct2:
-    if (value3 == 0) {
-      state = correct3;
-      flashRedQuick();
-    }
-    else if (value2 == 0 or value1 == 0 or value4 == 0){
-      state = wrong2;
-      flashRedQuick();
-    }
-    break;
+    case correct2:
+      if (i == 2) state = correct3;
+      else state = wrong3;
+      break;
 
     case correct3:
-    if (value4 == 0){
-      state = correct4;
-    
-      flashRedQuick();
-    }
-    else if (value2 == 0 or value3 == 0 or value1 == 0){
-      state = wrong4;
-      flashRedQuick();
-    }
-    break;
-    
-    case correct4:
-      digitalWrite(greenLight, HIGH);
-      digitalWrite(redLight, LOW);
-      digitalWrite(INTERRUPT_PIN, LOW);
+      if (i == 3) state = correct4;
+      else state = wrong4;
       break;
-      
+
+    case correct4:
+      // already solved
+      break;
+
     case wrong1:
-      if (value1 == 0 or value2 == 0 or value3 == 0 or value4 == 0){
-        state = wrong2;
-        flashRedQuick();
-       }
+      state = wrong2;
       break;
 
     case wrong2:
-    
-    if (value1 == 0 or value2 == 0 or value3 == 0 or value4 == 0){
-        state = wrong3;
-        flashRedQuick();
-       }
-       break;
+      state = wrong3;
+      break;
 
     case wrong3:
-      if (value1 == 0 or value2 == 0 or value3 == 0 or value4 == 0){
-        state = wrong4;
-        flashRedQuick();
-       }
-       break;
-      
+      state = wrong4;
+      break;
+
     case wrong4:
-      digitalWrite(redLight, HIGH);
-      digitalWrite(greenLight, LOW);
+      // stay failed
       break;
   }
 
-  
+  // ===================== OUTPUT RESPONSE =====================
+  if (state == correct4) {
+    digitalWrite(greenLight, HIGH);
+    digitalWrite(redLight, LOW);
+    digitalWrite(INTERRUPT_PIN, LOW);
+  }
+  else if (state == wrong4) {
+    digitalWrite(redLight, HIGH);
+    digitalWrite(greenLight, LOW);
+  }
+  else {
+    // intermediate feedback (quick flash)
+    digitalWrite(redLight, HIGH);
+    delay(30);
+    digitalWrite(redLight, LOW);
+  }
+
+  //Serial.println(state);
+
 }
 
-void flashRedQuick() {
-// Quick red flash for keypress feedback
-digitalWrite(redLight, HIGH);
-delay(30);
-digitalWrite(redLight, LOW);
+// ===================== RESET =====================
+void performFullReset() {
+  state = initial;
+  digitalWrite(redLight, LOW);
+  digitalWrite(greenLight, LOW);
+  digitalWrite(INTERRUPT_PIN, HIGH);
+  for (int i = 0; i < 4; i++) {
+    lastButtonState[i] = HIGH;
+  }
+
 }
 
 // ===================== I2C RECEIVE =====================
 void receiveEvent(int howMany) {
-  String word = "";
-  while (Wire.available()) {
+  if (Wire.available()) {
     char c = Wire.read();
-    word += c;
-  }
-  if (word == "S"){
-    PuzzleActive = true;
-  }
-  if (word == "R"){
-    PuzzleActive = false;
-    state = initial;
-    digitalWrite(INTERRUPT_PIN, HIGH);
-    digitalWrite(redLight, LOW);
-    digitalWrite(greenLight, LOW);
-      Serial.print("Reset");
 
-
+    if (c == 'S') {
+      PuzzleActive = true;
+      performFullReset();
+    } 
+    else if (c == 'R') {
+      PuzzleActive = false;
+      state = initial; // keep ISR safe
+    }
   }
-  Serial.print("Got message");
 }
